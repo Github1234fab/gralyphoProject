1d6de3da3e2e1ca1b2350543d0e78cd86b0716e0
Github1234fab <fabien.marceau@gmail.com>
2023-12-30 13:37:27 +0100

    v3

--- src/sections/Asection.svelte
+++ src/sections/Asection.svelte
@@ -1,19 +1,20 @@
 <script>
+  import background from "../Assets/png-gralypho/09.png";
 </script>
 
 <section class="A-section">
   <div class="bar"></div>
   <div class="wrapper-title">
     <!-- <h1>Sites internet et applications sur mesures. <br> Boostez votre présence en ligne grâce aux outils digitaux.</h1> -->
-    <h1>
-      Nous aidons les entreprises à décoller vers le succès grâce à nos conceptions de Sites Web, 100% sur mesure.
-    </h1>
+    <h1>Optez pour nos solutions digitales 100% sur mesure.</h1>
     <!-- <h2> Offrez une solution d'exception à votre activité, grâce  à notre expertise en développement web</h2> -->
     <h2>
-      Simplifiez vos opérations, augmentez votre visibilité et offrez à vos clients, une expérience exceptionnelle ! 
+      Simplifiez vos opérations, augmentez votre visibilité et offrez à vos clients une expérience exceptionnelle !
     </h2>
-    <a class="link" href="/#sectionE">Découvrir</a>
+      <a class="link" href="/#sectionE">Découvrir</a>
+    <img src={background} alt="" class="background" />
   </div>
+
 </section>
 
 <style>
@@ -30,44 +31,46 @@
     grid-template-rows: auto;
     height: 700px;
     font-size: 16px;
-    background-image: url(../Assets/png-gralypho/23.png);
-    background-size: cover;
-    background-position: center;
   }
 
   .wrapper-title {
     grid-column: 2/12;
-    background-color: rgba(5, 5, 42, 0.687);
-    height: 400px;
+    background-color: var(--colorN);
+    height: 500px;
     width: 100%;
     border-radius: 20px;
-    margin-top: 100px;
+    margin-top: 20px;
+    z-index: 0;
   }
 
   .A-section h1 {
     word-wrap: break-word;
-    z-index: 4;
+    z-index: 1;
     text-align: left;
     font-family: epilogue;
     font-weight: 500;
     font-size: 1.8em;
     color: white;
-    line-height: 35px;
+    line-height: 28px;
     padding: 30px;
-    margin-top: 30px;
+    margin-top: 0px;
   }
 
   .A-section h2 {
     margin-top: 0px;
-    z-index: 2;
+    z-index: 1;
     position: relative;
     font-family: epilogue;
     font-weight: 300;
     word-wrap: break-word;
     text-align: left;
-    font-size: 1em;
+    font-size: 1.1em;
     color: var(--colorC);
     padding: 30px;
+  }
+  .background {
+    width: 100%;
+    z-index: 0;
   }
 
   .link {
@@ -75,45 +78,51 @@
     z-index: 2;
     justify-content: center;
     align-items: center;
-    width: 30%;
-    height: 3em;
+    width: 50%;
+    height: 3.1em;
     background-color: var(--colorJ);
     border: none;
     border-radius: 40px;
     color: white;
     text-decoration: none;
     font-family: epilogue;
-    font-weight: 400;
-    font-size: 1em;
-    box-shadow:  2px 2px 5px var(--colorO);
+    font-weight: 700;
+    font-size: 1.3em;
+    box-shadow: inset 2px 2px 5px var(--colorO);
     margin-left: 30px;
+    margin-top:  20px;
+    margin-bottom: 30px;
+    padding: 30px;
+    transition: 0.3s ease-in-out;
+  }
+  .link:hover{
+    background-color: var(--colorI);
+    transform: scale(0.9);
   }
 
-  @media screen and (max-width: 768px){
-    .A-section{
+  @media screen and (max-width: 768px) {
+    .A-section {
       font-size: 13px;
-     
     }
-    .wrapper-title{
-       height: 410px;
+    .wrapper-title {
+      height: 410px;
     }
-     .A-section h2 {
- line-height: 25px;
- margin-top: -40px;
-  }
+    .A-section h2 {
+      line-height: 25px;
+      margin-top: -40px;
+    }
   }
 
-    @media screen and (max-width: 590px){
-    .A-section{
+  @media screen and (max-width: 590px) {
+    .A-section {
       font-size: 11px;
-     
     }
-    .wrapper-title{
-       height: 450px;
+    .wrapper-title {
+      height: 450px;
     }
-     .A-section h2 {
- line-height: 25px;
- margin-top: -40px;
-  }
+    .A-section h2 {
+      line-height: 25px;
+      margin-top: -40px;
+    }
   }
 </style>
